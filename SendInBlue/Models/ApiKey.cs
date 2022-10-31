a449fb702a316d03285e358812fa66ab41fdac1e
katreen100 <katyebraheem@gmail.com>
2022-10-31 11:50:55 +0200

    change api key

--- SendInBlue/Models/ApiKey.cs
+++ SendInBlue/Models/ApiKey.cs
@@ -9,6 +9,6 @@ namespace ConsoleApp1.Models
      public class ApiKey
     {
 
-        public string Apikey { get; } = "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";
+        public string Apikey { get; } = "add your api key";
     }
 }
